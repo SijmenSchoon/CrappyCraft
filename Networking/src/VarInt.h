02dbd03c15c55f722836b8c7a10fc921638306bf
Sijmen Schoon <vijfhoek35@gmail.com>
2014-04-21 19:05:07 +0200

    Added travis file, included VarInt class

---
+++ Networking/src/VarInt.h
@@ -0,0 +1,16 @@
+#ifndef VAR_INT_H
+#define VAR_INT_H
+
+#include <vector>
+
+class VarInt
+{
+public:
+    VarInt();
+    VarInt(char *data);
+    ~VarInt();
+
+    std::vector<char> mData;
+};
+
+#endif
